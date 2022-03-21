7a0bba0e1753065c5e2e94807fd7388eb412512a
gianlutech <gianluca.ingrao95@gmail.com>
2022-03-21 20:49:18 +0100

    fixed $books white $movie in forelse

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,17 +2,15 @@
 
 @section('content')
 
-<div class="d-flex">
-@forelse($books as $book) 
-<div class="card" style="width: 18rem;">
+<div class="d-flex flex-wrap justify-content-center">
+@forelse($movies as $movie) 
+<div class="card m-3 w-25">
   <div class="card-body">
     <h5 class="card-title">{{ $movie->title}}</h5>
     <h6 class="card-subtitle mb-2 text-muted">{{ $movie->original_title}}</h6>
     <p class="card-text">{{ $movie->nationality}}</p>
     <p class="card-text">{{ $movie->date}}</p>
     <p class="card-text">{{ $movie->vote}}</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
     @empty
